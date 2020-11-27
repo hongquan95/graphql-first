2e790efafaa4e8c548f33e6f014f979c10051754
hongquan95 <hongquan95.tran@gmail.com>
2020-11-27 11:32:07 +0700

    server

---
+++ server/app.js
@@ -0,0 +1,29 @@
+require('dotenv-safe').config();
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema')
+const mongoose = require('mongoose')
+const mongoURI = process.env.MONGO_URI;
+mongoose
+    .connect(mongoURI, {
+        useCreateIndex: true,
+        keepAlive: 1,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+    })
+    .then(() => console.log('mongoDB connected...'));
+
+// print mongoose logs in dev env
+if (process.env.NODE_ENV === 'development') {
+    mongoose.set('debug', true);
+}
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true
+}))
+app.listen(PORT, () => {
+    console.log(`Listen on port ${PORT}`);
+})
